e01377612d8fcb1431a71938be1fea71f7d1fd3e
Dr.Neil Watts <58720414+minue@users.noreply.github.com>
2024-11-08 20:56:59 +0900

    7회차 강의 내용 추가

---
+++ 7회차 강의/move.bat
@@ -0,0 +1,12 @@
+chcp 65001
+
+call tsc
+
+rmdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\UI TEST7[B]" /s /q
+rmdir  "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\UI TEST7[R]" /s /q
+
+mkdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\UI TEST7[B]"
+mkdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\UI TEST7[R]"
+
+xcopy ".\UI 강의\behavior_pack" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\UI TEST7[B]" /E /y
+xcopy ".\UI 강의\resource_pack" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\UI TEST7[R]" /E /y
