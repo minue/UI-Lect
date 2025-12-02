7406191db29b07209c491333913687495f23a81a
Dr.Neil Watts <58720414+minue@users.noreply.github.com>
2025-12-03 05:58:38 +0900

    update

--- 8회차 강의(텔레포트 코어)_미완성/move.bat
+++ 8회차 강의(텔레포트 코어)_미완성/move.bat
@@ -2,11 +2,11 @@ chcp 65001
 
 call tsc
 
-rmdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TP Core[B]" /s /q
-rmdir  "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TP Core[R]" /s /q
+rmdir "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\TP Core[B]" /s /q
+rmdir  "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\TP Core[R]" /s /q
 
-mkdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TP Core[B]"
-mkdir "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TP Core[R]"
+mkdir "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\TP Core[B]"
+mkdir "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\TP Core[R]"
 
-xcopy ".\UI 강의\behavior_pack" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TP Core[B]" /E /y
-xcopy ".\UI 강의\resource_pack" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TP Core[R]" /E /y
+xcopy ".\UI 강의\behavior_pack" "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\TP Core[B]" /E /y
+xcopy ".\UI 강의\resource_pack" "C:\Users\USER\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\TP Core[R]" /E /y
