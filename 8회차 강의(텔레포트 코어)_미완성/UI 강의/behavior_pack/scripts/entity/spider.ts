428cbbc4119a0e18ec9b4d5f01f6f3adbeef0f78
minue <minue1003@naver.com>
2025-12-29 22:11:42 +0900

    update

--- 8회차 강의(텔레포트 코어)_미완성/UI 강의/behavior_pack/scripts/entity/spider.ts
+++ 8회차 강의(텔레포트 코어)_미완성/UI 강의/behavior_pack/scripts/entity/spider.ts
@@ -1,4 +1,19 @@
-import { Dimension, Entity, EntityComponentTypes, EntityTameableComponent, Player, PlayerInteractWithEntityAfterEvent, world } from "@minecraft/server";
+import { Block, BlockType, BlockTypes, Container, Dimension, DimensionType, DimensionTypes, Entity, EntityComponentTypes, EntityDamageCause, EntityHealthComponent, EntityHurtAfterEvent, EntityInventoryComponent, EntityTameableComponent, ItemStack, Player, PlayerInteractWithEntityAfterEvent, world } from "@minecraft/server";
+
+
+enum ACT {
+    IGNORE = 0,
+    ATTACK = 1,
+    KIDNAP = 2,
+    MINE = 3,
+    MARK = 4
+}
+
+enum TYPE {
+    BLOCK = 0,
+    ENTITY = 1,
+    PLACE = 2
+}
 
 function getSpiders(player: Player) {
     const dimension: Dimension = player.dimension
@@ -47,4 +62,116 @@ export function getSlaveSorted(player: Player) {
         const bNum = slaveNum!.getScore(b.scoreboardIdentity!)!
         return aNum - bNum
     })
+}
+
+class Log {
+    id: string
+    dimension: Dimension
+    x: number
+    y: number
+    z: number
+    constructor(id: string, x: number, y: number, z: number, dimension: string) {
+        this.x = x
+        this.y = y
+        this.z = z
+        this.id = id
+        this.dimension = world.getDimension(dimension)
+    }
+    checkNearest(){
+        this.dimension.getBlockAbove(this,{includePassableBlocks: true})
+    }
+    teleport(player: Player) {
+        player.teleport(this, {dimension: this.dimension})
+    }
+}
+
+class DataChip {
+
+    type: number
+    id: string
+    act: number
+    x: number
+    y: number
+    z: number
+    hp: number
+
+    constructor(item: ItemStack) {
+        const lore = item.getLore()
+        this.type = ACT[lore[0].replace("mode: ", "")]
+        this.id = lore[1].replace("id: ", "")
+        this.act = ACT[lore[2]].replace("act: ", "")
+        this.x = parseInt(lore[3].replace("x: ", "") == "None" ? "0" : lore[3].replace("x: ", ""))
+        this.y = parseInt(lore[4].replace("y: ", "") == "None" ? "0" : lore[4].replace("x: ", ""))
+        this.z = parseInt(lore[5].replace("z: ", "") == "None" ? "0" : lore[5].replace("x: ", ""))
+        this.hp = parseInt(lore[6].replace("hp: ", "") == "None" ? "-1" : lore[6].replace("x: ", ""))
+    }
+
+    setLog(item: ItemStack, str: string): ItemStack{
+        const log: string[] = item.getLore()
+        if(log.length > 50) {
+            return item
+        }
+        log.push(str)
+        item.setLore(log)
+        return item
+    }
+
+    getLog(item: ItemStack): string[] {
+        const log: string[] = item.getLore()
+        return log.slice(7)
+    }
+
+    makeLogStr(block: Block): string{
+        if(block.typeId == this.id){
+            const str = `x: ${block.x}/y: ${block.y}/z: ${block.z}/type: ${block.typeId}/dimension: ${block.dimension.id}`
+            return str
+        }
+        return ""
+    }
+
+    logInterpret(str: string): Log {
+        const log: string[] = str.split("/")
+        const x = parseFloat(log[0].replace("x: ", ""))
+        const y = parseFloat(log[1].replace("y: ", ""))
+        const z = parseFloat(log[2].replace("z: ", ""))
+        const typeid = log[3].replace("type: ", "")
+        const dimension = log[4].replace("dimension: ", "")
+        return new Log(typeid, x,y,z,dimension)
+    }
+}
+
+class React {
+    strArr: string[]
+    spider: Entity
+
+    constructor(str: string) {
+    }
+
+    basicEntityReact(ev: PlayerInteractWithEntityAfterEvent) {
+        const container: Container = (ev.target.getComponent(EntityComponentTypes.Inventory) as EntityInventoryComponent).container
+        let react: string[]
+        for(let index = 0 ; index < container.size; index++){
+            let itemStack = container.getItem(index);
+            if(itemStack?.typeId != "watts:data_chip"){
+                continue
+            }
+            container.getItem
+        }
+    }
+
+    spiderAct(ent: Entity) {
+    }
+    spiderAttack(ent: Entity) {
+        if(this.spider.getProperty("watts:has_target")){
+            return;
+        }
+        this.spider.applyDamage(0, { cause: EntityDamageCause.entityAttack, damagingEntity: ent })
+        this.spider.triggerEvent("watts:has_target")
+    }
+    spiderKidnap(ent: Entity) {
+    }
+
+    attackEvent(ev: EntityHurtAfterEvent) {
+        this.spiderAct(ev.hurtEntity)
+    }
 }
--- 8회차 강의(텔레포트 코어)_미완성/UI 강의/behavior_pack/scripts/form/spider.ts
+++ 8회차 강의(텔레포트 코어)_미완성/UI 강의/behavior_pack/scripts/form/spider.ts
@@ -18,92 +18,12 @@ const setMode = new ActionFormData()
 const get_log = new ModalFormData()
     .submitButton("watts.ui.log.spider.confirm")
 
-class React {
-    static readonly IGNORE = 0
-    static readonly ATTACK = 1
-    static readonly KIDNAP = 2
-
-    id: string
-    act: number
-    x: number
-    y: number
-    z: number
-    hp: number
-    strArr: string[]
-    spider: Entity
-
-    constructor(str: string) {
-        this.strArr = str.split(".")
-        this.id = this.strArr[0]
-        this.act = parseInt(this.strArr[1])
-        if (this.act == React.KIDNAP) {
-            this.x = parseInt(this.strArr[2])
-            this.y = parseInt(this.strArr[3])
-            this.z = parseInt(this.strArr[4])
-            this.hp = parseInt(this.strArr[5])
-        }
-    }
-
-    spiderAct(ent: Entity) {
-        if (this.act == React.IGNORE) {
-            return
-        }
-        const health = (ent.getComponent(EntityComponentTypes.Health) as EntityHealthComponent).currentValue
-        if (this.act == React.KIDNAP && this.hp == -1) {
-            this.spiderKidnap(ent)
-            return;
-        }
-        if ((this.act == React.KIDNAP && this.hp >= health) || (this.act == React.ATTACK)) {
-            this.spiderAttack(ent)
-            return;
-        }
-    }
-    spiderAttack(ent: Entity) {
-        if(this.spider.getProperty("watts:has_target")){
-            return;
-        }
-        this.spider.applyDamage(0, { cause: EntityDamageCause.entityAttack, damagingEntity: ent })
-        this.spider.triggerEvent("watts:has_target")
-    }
-    spiderKidnap(ent: Entity) {
-        const hp = (ent.getComponent(EntityComponentTypes.Health) as EntityHealthComponent).currentValue
-        const vx = ent.location.x, vy = ent.location.y, vz = ent.location.z
-        const distance = Math.sqrt((vx - this.x) ^ 2 + (vy - this.y) ^ 2 + (vz - this.z) ^ 2)
-        const consume = Math.min(100, Math.round(5 + 2 * distance) + Math.round(hp / 5))
-        const energyObj = world.scoreboard.getObjective("energy")
-        const identity = this.spider.scoreboardIdentity
-        const energy = energyObj?.getScore(identity!)!
-
-        if (consume > energy) {
-            return
-        }
-
-        energyObj?.setScore(identity!, energy - consume)
-        this.spider.triggerEvent("watts:kidnap_mode")
-    }
-
-    attackEvent(ev: EntityHurtAfterEvent) {
-        this.spiderAct(ev.hurtEntity)
-    }
-}
-
 function basicEntity(ev: PlayerInteractWithEntityAfterEvent) {
     const setModeBasicEntity = new ActionFormData()
     const entityTypes = EntityTypes.getAll()
 
     entityTypes.forEach((ent) => {
         setModeBasicEntity.button(`${ent.id}`)
-    })
-}
-
-function basicEntityReact(ev: PlayerInteractWithEntityAfterEvent) {
-    const tags: string[] = ev.target.getTags()
-    let react: string[]
-    tags.forEach((tag) => {
-        if (tag.startsWith("watts.spider.react=")) {
-            react = tag.replace("watts.spider.react=", "").split("/")
-            return;
-        }
     })
 }
 
@@ -114,8 +34,4 @@ export function spiderUIFunc(ev: PlayerInteractWithEntityAfterEvent) {
         } else {
         }
     })
-}
-
-function constructor(str: any, string: any) {
-    throw new Error("Function not implemented.");
 }
